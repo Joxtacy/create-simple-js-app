a50ce82d519458980507874200287f807e3c9634
Jesper Hasselquist <jesper.hasselquist@telavox.se>
2019-10-28 21:03:24 +0100

    Add inquirer and ask for project name

--- bin/csja.js
+++ bin/csja.js
@@ -3,6 +3,7 @@
 const path = require("path");
 const fs = require("fs-extra");
 const execa = require("execa");
+const inquirer = require("inquirer");
 const ora = require("ora");
 const oraSpinner = ora();
 
@@ -24,6 +25,7 @@ const getDependencies = deps =>
         // exclude the plugin only used in this file, nor relevant to the boilerplate
         .replace(/ora[^\s]+/g, "")
         .replace(/fs-extra[^\s]+/g, "")
+        .replace(/inquirer[^\s]+/g, "")
         .replace(/execa[^\s]+/g, "");
 
 const scripts = `"build": "MODE=production webpack",
@@ -32,17 +34,40 @@ const scripts = `"build": "MODE=production webpack",
     "lint:fix": "eslint --ext .js --fix ./",
     "test": "jest --watchAll"`;
 
-const projectName = process.argv[2];
+
+async function validator(input) {
+    const regexp = /[A-Z0-9-_]/gi;
+    const found = input.match(regexp);
+    if (found && input && found.length === input.length) {
+        return true;
+    } else {
+        return "Invalid project name!";
+    }
+}
+const questions = [
+    {
+        type: "input",
+        name: "projectName",
+        message: "Write your project name. Can only contain [a-zA-Z0-9-_].",
+        validate: validator
+    }
+];
 
 async function createSimpleJsApp() {
+    let options;
     try {
-        await installEverything();
+        options = await inquirer.prompt(questions);
     } catch (error) {
-        await rollbackInstallation();
+    }
+
+    try {
+        await installEverything(options);
+    } catch (error) {
+        await rollbackInstallation(options.projectName);
     }
 }
 
-async function rollbackInstallation() {
+async function rollbackInstallation(projectName) {
     try {
         oraSpinner.start("Rolling back changes...");
         await execa("rm", ["-rf", projectName]);
@@ -52,11 +77,11 @@ async function rollbackInstallation() {
     }
 }
 
-async function installEverything() {
+async function installEverything(options) {
     // Create project folder
     try {
         oraSpinner.start("Creating project folder");
-        await execa("mkdir", [projectName]);
+        await execa("mkdir", [options.projectName]);
         oraSpinner.succeed();
     } catch (error) {
         oraSpinner.fail();
@@ -66,7 +91,7 @@ async function installEverything() {
     //Initialize npm
     try {
         oraSpinner.start("Initializing npm");
-        await execa("npm", ["init", "--yes"], { cwd: projectName });
+        await execa("npm", ["init", "--yes"], { cwd: options.projectName });
         oraSpinner.succeed();
     } catch (error) {
         oraSpinner.fail();
@@ -74,7 +99,7 @@ async function installEverything() {
     }
 
     // Add scripts to package.json
-    const packageJsonFile = `${projectName}/package.json`;
+    const packageJsonFile = `${options.projectName}/package.json`;
     try {
         oraSpinner.start("Adding scripts");
         const file = await fs.readFile(packageJsonFile);
@@ -99,7 +124,7 @@ async function installEverything() {
     try {
         oraSpinner.start("Copying configuration files");
         for (let i = 0; i < filesToCopy.length; i += 1) {
-            await fs.copy(path.join(__dirname, `../${filesToCopy[i]}`), `${projectName}/${filesToCopy[i]}`);
+            await fs.copy(path.join(__dirname, `../${filesToCopy[i]}`), `${options.projectName}/${filesToCopy[i]}`);
         }
         oraSpinner.succeed();
     } catch (error) {
@@ -110,7 +135,7 @@ async function installEverything() {
     // Copy __mocks__
     try {
         oraSpinner.start("Copying __mocks__");
-        await fs.copy(path.join(__dirname, "../__mocks__"), `${projectName}/__mocks__`);
+        await fs.copy(path.join(__dirname, "../__mocks__"), `${options.projectName}/__mocks__`);
         oraSpinner.succeed();
     } catch (error) {
         oraSpinner.fail();
@@ -122,19 +147,20 @@ async function installEverything() {
         const devDeps = getDependencies(packageJson.devDependencies);
         const deps = getDependencies(packageJson.dependencies);
         oraSpinner.start("Installing devDependencies");
-        await execa.command(`npm i --save-dev ${devDeps}`, { cwd: projectName });
+        await execa.command(`npm i --save-dev ${devDeps}`, { cwd: options.projectName });
         oraSpinner.succeed();
         oraSpinner.start("Installing dependencies");
-        await execa.command(`npm i --save-dev ${deps}`, { cwd: projectName });
+        await execa.command(`npm i --save-dev ${deps}`, { cwd: options.projectName });
         oraSpinner.succeed();
     } catch (error) {
         oraSpinner.fail();
+        console.error("ERROR:", error);
         throw error;
     }
 
     try {
         oraSpinner.start("Copying src");
-        await fs.copy(path.join(__dirname, "../src"), `${projectName}/src`);
+        await fs.copy(path.join(__dirname, "../src"), `${options.projectName}/src`);
         oraSpinner.succeed();
     } catch (error) {
         oraSpinner.fail();
